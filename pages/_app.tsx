3ea540b8521ee8eb6ee6aedbace913eac71f72bf
Bandit Silachai <mbandit001@yahoo.co.th>
2022-07-27 11:04:33 +0700

    🔖 mantine v5.0.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Mantine Next.JS example</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
